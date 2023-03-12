69c6b5203c4e23f65e6133a5182ec2873fe229e1
Valery Verkhoturov <verkhoturov.valery@icloud.com>
2023-03-12 23:18:58 +0300

    fix: small screen size sidebar menu

--- components/LayoutWithDrawer.tsx
+++ components/LayoutWithDrawer.tsx
@@ -170,29 +170,8 @@ export default function LayoutWithDrawer(props: LayoutProps) {
                       <NinjaXUnion />
                     </div>
                     <nav className='mt-5 space-y-1 px-2'>
-                      {navigation.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className={classNames(
-                            item.current
-                              ? 'bg-gray-100 text-gray-900'
-                              : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                            'group flex items-center rounded-md px-2 py-2 text-base font-medium',
-                          )}
-                        >
-                          <item.icon
-                            className={classNames(
-                              item.current
-                                ? 'text-gray-500'
-                                : 'text-gray-400 group-hover:text-gray-500',
-                              'mr-4 h-6 w-6 flex-shrink-0',
-                            )}
-                            aria-hidden='true'
-                          />
-                          {item.name}
-                        </a>
-                      ))}
+                      {formatNavigation(navigation)}
+                      {isSuperuser && formatNavigation(superuserNavigation)}
                     </nav>
                   </div>
 
