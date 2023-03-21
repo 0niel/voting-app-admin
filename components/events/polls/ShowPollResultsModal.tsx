f7f3ffb27384c5bf91fc18590e203295fde72a03
Валера <70258211+ValeryVerkhoturov@users.noreply.github.com>
2023-03-21 17:35:17 +0300

    Add pie chart for poll results (#69)

--- components/events/polls/ShowPollResultsModal.tsx
+++ components/events/polls/ShowPollResultsModal.tsx
@@ -1,5 +1,7 @@
 import { Databases, Query } from 'appwrite'
+import { ArcElement, Chart, Legend, Tooltip } from 'chart.js'
 import React, { useEffect, useState } from 'react'
+import { Pie } from 'react-chartjs-2'
 import { toast } from 'react-hot-toast'
 
 import Modal from '@/components/modal/Modal'
@@ -12,9 +14,12 @@ import { useAppwrite } from '@/context/AppwriteContext'
 import { usePoll } from '@/context/PollContext'
 import { VoteDocument } from '@/lib/models/VoteDocument'
 
+Chart.register(ArcElement, Tooltip, Legend)
+
 export default function ShowPollResultsModal() {
   const { pollIdToShowResults, setPollIdToShowResults } = usePoll()
   const [votes, setVotes] = useState<Map<string, number>>(new Map())
+  const [pieData, setPieData] = useState<object>()
   const { client } = useAppwrite()
   const databases = new Databases(client)
 
@@ -32,6 +37,32 @@ export default function ShowPollResultsModal() {
         votesMap.set(vote.vote, (votesMap.get(vote.vote) || 0) + 1)
       })
       setVotes(votesMap)
+      setPieData({
+        labels: Array.from(votesMap.keys()),
+        datasets: [
+          {
+            label: 'голосов',
+            data: Array.from(votesMap.values()),
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      })
     }
 
     if (pollIdToShowResults !== undefined) {
@@ -58,13 +89,19 @@ export default function ShowPollResultsModal() {
               return (
                 <li
                   key={voteOption}
-                  className={Math.max(...Array.from(votes.values())) === count ? 'underline' : ''}
+                  className={Math.max(...Array.from(votes.values())) === count ? 'text-accent' : ''}
                 >
                   {voteOption} — {count} гол.
                 </li>
               )
             })}
           </ul>
+        )}
+        {pieData && (
+          <Pie
+            className='h-15 w-15 p-2' // @ts-ignore
+            data={pieData}
+          />
         )}
       </div>
     </Modal>
