3734347de7761ac7e3b8c4ba41786416662e4a4e
Valery Verkhoturov <verkhoturov.valery@icloud.com>
2023-03-31 23:49:17 +0300

    fix: fix plot min height, set title, add favicon

--- components/events/polls/ShowPollResultsModal.tsx
+++ components/events/polls/ShowPollResultsModal.tsx
@@ -1,25 +1,37 @@
 import { HandRaisedIcon } from '@heroicons/react/24/outline'
 import { Databases, Query } from 'appwrite'
+import { formatTime } from 'jest-util'
 import dynamic from 'next/dynamic'
+import { useRouter } from 'next/router'
 import { Data } from 'plotly.js'
 import React, { useEffect, useState } from 'react'
 import { toast } from 'react-hot-toast'
+import wrap from 'word-wrap'
 
 import Modal from '@/components/modal/Modal'
 import {
+  appwriteEventsCollection,
   appwriteListVotesLimit,
+  appwritePollsCollection,
   appwriteVotesCollection,
   appwriteVotingDatabase,
 } from '@/constants/constants'
 import { useAppwrite } from '@/context/AppwriteContext'
 import { usePoll } from '@/context/PollContext'
+import { formatDate } from '@/lib/formatDate'
+import { EventDocument } from '@/lib/models/EventDocument'
+import { PollDocument } from '@/lib/models/PollDocument'
 import { VoteDocument } from '@/lib/models/VoteDocument'
 import { pluralForm } from '@/lib/pluralForm'
 
 const Plot = dynamic(() => import('react-plotly.js'), { ssr: false })
 
 export default function ShowPollResultsModal() {
+  const router = useRouter()
+  const { eventID } = router.query
   const { pollIdToShowResults, setPollIdToShowResults } = usePoll()
+  const [event, setEvent] = useState<EventDocument>()
+  const [poll, setPoll] = useState<PollDocument>()
   const [votes, setVotes] = useState<Map<string, number>>(new Map())
   const [barData, setBarData] = useState<Data[]>([])
   const [loading, setLoading] = useState(true)
@@ -32,6 +44,19 @@ export default function ShowPollResultsModal() {
     const votesMap = new Map<string, number>()
     const fetchVotes = async () => {
       setLoading(true)
+      const event = (await databases.getDocument(
+        appwriteVotingDatabase,
+        appwriteEventsCollection,
+        eventID as string,
+      )) as EventDocument
+      setEvent(event)
+
+      const poll = (await databases.getDocument(
+        appwriteVotingDatabase,
+        appwritePollsCollection,
+        pollIdToShowResults!,
+      )) as PollDocument
+      setPoll(poll)
       ;(
         (
           await databases.listDocuments(appwriteVotingDatabase, appwriteVotesCollection, [
@@ -46,13 +71,21 @@ export default function ShowPollResultsModal() {
 
       const votesSum = Array.from(votesMap.values()).reduce((partialSum, a) => partialSum + a, 0)
       setVotesSum(votesSum)
-      setSimpleMajority(Math.ceil(votesSum / 2))
+      if (votesSum > 1 && votesSum % 2 === 0) {
+        setSimpleMajority(Math.ceil(votesSum / 2) + 1)
+      } else {
+        setSimpleMajority(Math.ceil(votesSum / 2))
+      }
 
       setBarData([
         {
           y: Array.from(votesMap.values()),
           x: Array.from(votesMap.keys()),
+          text: Array.from(votesMap.values()).map(String),
           type: 'bar',
+          marker: {
+            color: 'rgb(79, 70, 229)',
+          },
         },
       ])
       setLoading(false)
@@ -73,53 +106,76 @@ export default function ShowPollResultsModal() {
       hideCancelButton={true}
     >
       <div className='p-2'>
-        {Array.from(votes.values()).length === 0 ? (
-          'Голосов нет.'
-        ) : (
-          <div>
-            {Array.from(votes, ([voteOption, count], index) => (
-              <React.Fragment key={voteOption}>
-                <div className='my-2 flex'>
-                  <HandRaisedIcon className='mt-1 mr-1 h-4 w-4' />
-                  {voteOption} — <span className='mx-1 font-semibold'>{count}</span>
-                  {pluralForm(count, ['голос', 'голоса', 'голосов'])}
-                </div>
-                <hr className='my-1 h-0.5 border-t-0 bg-neutral-100 opacity-100' />
-                {/*{index !== Array.from(votes.values()).length - 1 && (*/}
-                {/*  <hr className='my-1 h-0.5 border-t-0 bg-neutral-100 opacity-100' />*/}
-                {/*)}*/}
-              </React.Fragment>
-            ))}
-          </div>
-        )}
         {!loading ? (
-          <Plot
-            data={barData}
-            layout={{
-              height: 300,
-              width: 400,
-              uirevision: 'true',
-              shapes: [
-                {
-                  type: 'line',
-                  xref: 'paper',
-                  x0: 0,
-                  y0: simpleMajority,
-                  x1: 1,
-                  y1: simpleMajority,
-                  line: {
-                    color: 'rgb(255, 0, 0)',
-                    width: 2,
-                    dash: 'solid',
+          Array.from(votes.values()).length === 0 ? (
+            'Голосов нет.'
+          ) : (
+            <div>
+              {Array.from(votes, ([voteOption, count], index) => (
+                <React.Fragment key={voteOption}>
+                  <div className='my-2 flex'>
+                    <HandRaisedIcon className='mt-1 mr-1 h-4 w-4' />
+                    {voteOption} — <span className='mx-1 font-semibold'>{count}</span>
+                    {pluralForm(count, ['голос', 'голоса', 'голосов'])}
+                  </div>
+                  <hr className='my-1 h-0.5 border-t-0 bg-neutral-100 opacity-100' />
+                  {/*{index !== Array.from(votes.values()).length - 1 && (*/}
+                  {/*  <hr className='my-1 h-0.5 border-t-0 bg-neutral-100 opacity-100' />*/}
+                  {/*)}*/}
+                </React.Fragment>
+              ))}
+            </div>
+          )
+        ) : (
+          [1, 2, 3].map((_, index) => (
+            <div key={index} className='animate-pulse'>
+              <div className='my-2 h-3.5 w-52 rounded-full bg-gray-200' />
+              <hr className='my-3 h-0.5 border-t-0 bg-neutral-100 opacity-100' />
+            </div>
+          ))
+        )}
+        <div className='min-h-[300px]'>
+          {!loading ? (
+            <Plot
+              data={barData}
+              layout={{
+                title: {
+                  text: wrap(
+                    `Гистограмма голосования ${event?.name.trim()}\n«${poll?.question.trim()}»`,
+                    {
+                      width: 40,
+                      newline: '<br>',
+                    },
+                  ),
+                  font: {
+                    size: 12,
                   },
                 },
-              ],
-            }}
-            config={{ displaylogo: false }}
-          />
-        ) : (
-          <div className='m-auto my-20 h-36 w-60 animate-pulse rounded-xl bg-gray-200' />
-        )}
+                height: 300,
+                width: 400,
+                uirevision: 'true',
+                shapes: [
+                  {
+                    type: 'line',
+                    xref: 'paper',
+                    x0: 0,
+                    y0: simpleMajority,
+                    x1: 1,
+                    y1: simpleMajority,
+                    line: {
+                      color: 'rgb(255, 0, 0)',
+                      width: 3,
+                      dash: 'solid',
+                    },
+                  },
+                ],
+              }}
+              config={{ displaylogo: false }}
+            />
+          ) : (
+            <div className='m-auto my-20 h-36 w-60 animate-pulse rounded-xl bg-gray-200' />
+          )}
+        </div>
       </div>
       <hr className='my-3 h-0.5 border-t-0 bg-neutral-100 opacity-100' />
       <div className='flex'>
